7089b3469892ef5ca2e6cba404e4aacad8a76d7a
cuewen <105962560+cuewen@users.noreply.github.com>
2024-02-11 01:41:02 +0700

    AES keys encrypted with RSA

--- client.py
+++ client.py
@@ -5,8 +5,16 @@ from threading import Thread
 from datetime import datetime
 from time import perf_counter
 from colorama import Fore, init, Back
-from encryption import encrypt, decrypt, generate_key
+from encryption import (
+    encrypt_aes, 
+    decrypt_aes, 
+    generate_key, 
+    send_encrypted,
+    recv_encrypted
+    )
 from base64 import b64decode, b64encode
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 init()
 
@@ -32,16 +40,24 @@ def signin():
     password = input("Enter your password: ")
     s.send(name.encode("utf-8"))
     try:
-        server_resp = decrypt(s.recv(1024)).decode("utf-8")
+        with open(f"keys/{name}_private.pem", "rb") as f:
+            my_pvtkey = RSA.import_key(f.read())
+        my_cipher = PKCS1_OAEP.new(my_pvtkey)
+        data = s.recv(2048).decode("utf-8")
+        data, aes, pub = recv_encrypted(data)
+        aes = my_cipher.decrypt(aes)
+        server_resp = decrypt_aes(data, aes).decode("utf-8")
         salt, challenge = server_resp.split("|")
         key = generate_key(password, b64decode(salt.encode("utf-8")))
-        response = decrypt(b64decode(challenge.encode("utf-8")), key).decode("utf-8")
+        response = decrypt_aes(b64decode(challenge.encode("utf-8")), key).decode("utf-8")
         if response == "OK":
             s.send("OK".encode("utf-8"))
-            s.send(encrypt(f"{cli_color}{name} connected.{Fore.RESET}".encode()))
+            connected = encrypt_aes(f"{cli_color}{name} connected.{Fore.RESET}"\
+                                    .encode("utf-8"))
+            s.send(send_encrypted(connected, server_pubkey).encode("utf-8"))
             print(f"Welcome, {name}!")
-            return name
-    except:
+            return name, my_cipher
+    except Exception:
         print("Invalid username or password.")
         s.close()
         exit(1)
@@ -69,14 +85,25 @@ def signup():
             print("Password is too short.")
     salt = os.urandom(16)
     hash = generate_key(password, salt)
-    s.send(encrypt(f"{name}|{b64encode(hash).decode("utf-8")}|{b64encode(salt).decode("utf-8")}".encode("utf-8")))
+    rsa_keys = RSA.generate(2048)
+    pubkey = rsa_keys.public_key().export_key()
+    pvtkey = rsa_keys.export_key()
+    with open(f"keys/{name}_private.pem", "wb") as f:
+        f.write(pvtkey)
+    data = encrypt_aes(f"{name}|{b64encode(hash).decode("utf-8")}|\
+            {b64encode(salt).decode("utf-8")}|\
+            {pubkey.decode("utf-8")}".encode("utf-8"))
+    s.send(send_encrypted(data, server_pubkey).encode("utf-8"))
     print(s.recv(1024).decode("utf-8"))
     return signin()
 
 def listen_for_messages():
     while True:
         try:
-            msg = decrypt(s.recv(1024)).decode("utf-8")
+            data = s.recv(4096).decode("utf-8")
+            data, aes, pub = recv_encrypted(data)
+            aes = my_cipher.decrypt(aes)
+            msg = decrypt_aes(data, aes).decode("utf-8")
             msg = msg.replace(separator_token, ": ", 1)
             if msg[:8] == "[Server]":
                 print(f"\n{Fore.LIGHTGREEN_EX}{msg}{Fore.RESET}")
@@ -88,23 +115,22 @@ def listen_for_messages():
 s = socket.socket()
 print(f"[*] Connecting to {SERVER_HOST}:{SERVER_PORT}")
 s.connect((SERVER_HOST, SERVER_PORT))
+server_pubkey = RSA.import_key(s.recv(1024))
 print("[+] Connected.")
 enter = input("!s to sign in | !r to sign up: ")
 if enter == "!s":
-    name = signin()
+    name, my_cipher = signin()
 elif enter == "!r":
-    name = signup()
+    name, my_cipher = signup()
 else:
     s.close()
     exit(1)
-
-    
 
 t = Thread(target=listen_for_messages, daemon=True)
 t.start()
 
 cmd_t_o = [0]
-
+timeout_warn = f"\n{Back.RED}{Fore.BLACK}[!] Command timeout 3s.{Fore.RESET}{Back.RESET}"
 while True:
     to_send = input()
     if to_send.lower() == "q":
@@ -115,7 +141,7 @@ while True:
     if to_send == "!userlist":
         t_o = perf_counter()
         if abs(t_o - cmd_t_o[0]) < 3:
-            print(f"\n{Back.RED}{Fore.BLACK}[!] Command timeout 3s.{Fore.RESET}{Back.RESET}")
+            print(timeout_warn)
         else:
             s.send(to_send.encode("utf-8"))
         cmd_t_o[0] = t_o
@@ -123,15 +149,16 @@ while True:
     if to_send == "!code":
         t_o = perf_counter()
         if abs(t_o - cmd_t_o[0]) < 3:
-            print(f"\n{Back.RED}{Fore.BLACK}[!] Command timeout 3s.{Fore.RESET}{Back.RESET}")
+            print(timeout_warn)
         else:
             s.send(to_send.encode("utf-8"))
         cmd_t_o[0] = t_o
         continue
-    
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     to_send = f"{cli_color}[{now}] {name}{separator_token}{to_send}{Fore.RESET}"
     print(t := to_send.replace(separator_token, ": ", 1))
-    s.send(encrypt(to_send.encode("utf-8")))
-s.send(encrypt(f"{cli_color}{name} disconnected.{Fore.RESET}".encode("utf-8")))
+    to_send = encrypt_aes(to_send.encode("utf-8"))
+    s.send(send_encrypted(to_send, server_pubkey).encode("utf-8"))
+dc = encrypt_aes(f"{cli_color}{name} disconnected.{Fore.RESET}".encode("utf-8"))
+s.send(send_encrypted(dc, server_pubkey).encode("utf-8"))
 s.close()
--- server.py
+++ server.py
@@ -1,46 +1,70 @@
 import socket
 from threading import Thread
-from encryption import encrypt, decrypt, generate_sha256
+from encryption import (
+    encrypt_aes, 
+    decrypt_aes, 
+    generate_sha256, 
+    send_encrypted, 
+    recv_encrypted
+    )
 from base64 import b64encode, b64decode
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
 SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 5002
-
+SERVER_RSA = RSA.generate(2048)
+s_cipher = PKCS1_OAEP.new(SERVER_RSA)
 users = {}
 
 f_codes = {
     "admin":\
-          "A408DB8643628EAC4C6474814F347EEA06EB51BE8108D3C461EE5DADE74A17ED"} 
-#server-admin-friend_code
+    "A408DB8643628EAC4C6474814F347EEA06EB51BE8108D3C461EE5DADE74A17ED"}
 
 client_sockets = set()
-authenticated_users = []
+authenticated_users = {}
 
 def listen_for_client(cs, username):
     while True:
         try:
-            msg = cs.recv(1024)
+            msg = cs.recv(4096).decode("utf-8")
             try:
-                if msg.decode("utf-8"):
-                    handle_command(msg, cs, username)
-                    continue
-            except:
-                pass
-            for client in client_sockets:
-                if client != cs:
-                    client.send(msg)
+                msg, aes, pub = recv_encrypted(msg)
+                if pub == SERVER_RSA.public_key():   
+                    dec_key = s_cipher.decrypt(aes)
+                    for client in client_sockets:
+                        if client != cs:
+                            name = ""
+                            for user, sock in authenticated_users.items():
+                                if sock == client:
+                                    name = user
+                                    break
+                            pubkey = users[name]["public"]
+                            client.send(send_encrypted(
+                                (msg, dec_key), pubkey
+                                ).encode("utf-8"))
+                else:
+                    cli = ""
+                    for user, sub_dict in users.items():
+                        if sub_dict['public'] == pub.export_key():
+                            cli = authenticated_users[user]
+                            cli.send(send_encrypted((msg, aes), pub).encode("utf-8"))
+                            break
+            except Exception as e:
+                handle_command(msg, cs, username)
+                continue
         except (OSError, ConnectionResetError):
             print("Socket is closed.")
             if cs in client_sockets:
                 client_sockets.remove(cs)
-            authenticated_users.remove(username)
+            del authenticated_users[username]
             del f_codes[username]
             cs.close()
             break
 
 def handle_command(cmd, cs, user=None):
     hr = "-" * 80
-    if cmd.decode("utf-8") == "!signup":
+    if cmd == "!signup":
         try:
             friend_code, friend = cs.recv(1024).decode("utf-8").split("|")
             if f_codes[friend] == friend_code:
@@ -48,23 +72,35 @@ def handle_command(cmd, cs, user=None):
         except (KeyError, ValueError):
             cs.send("reject".encode("utf-8"))
             return  
-        reg_info = cs.recv(1024)
-        reg_info = decrypt(reg_info).decode("utf-8")
-        name, passw, salt = reg_info.split("|")
+        reg_info = cs.recv(2048).decode("utf-8")
+        reg_info, aes, pub = recv_encrypted(reg_info)
+        aes = s_cipher.decrypt(aes)
+        reg_info = decrypt_aes(reg_info, aes).decode("utf-8")
+        name, passw, salt, pubkey = reg_info.split("|")
         if name not in users:
             cs.send(f"[+] You've successfully created an account!".encode("utf-8"))
             f_codes[friend] = generate_sha256()
-            users[name] = {"passw": b64decode(passw.encode("utf-8")), "salt": b64decode(salt)}
+            pubkey = pubkey.strip()
+            users[name] = {
+                "passw": b64decode(passw.encode("utf-8")), 
+                "salt": b64decode(salt),
+                "public": pubkey.encode("utf-8")
+            }
         else:
             cs.send(f"[-] {name} is not available.".encode("utf-8"))
             pass
 ### Chat commands
-    elif cmd.decode("utf-8") == "!userlist":
-        userlist = f"[Server]\nUser list:\n{hr}\n{"\n".join(authenticated_users)}\n{hr}"
-        cs.send(encrypt(userlist.encode("utf-8")))
-    elif cmd.decode("utf-8") == "!code":
+    elif cmd == "!userlist":
+        user_pub = users[user]["public"]
+        userlist =\
+         f"[Server]\nUser list:\n{hr}\n{"\n".join(authenticated_users.keys())}\n{hr}"
+        cs.send(send_encrypted(encrypt_aes(userlist.encode("utf-8")), user_pub)\
+                .encode("utf-8"))
+    elif cmd == "!code":
         code = f"[Server]\nYour friend code:\n{hr}\n{f_codes[user]}\n{hr}"
-        cs.send(encrypt(code.encode("utf-8")))
+        user_pub = users[user]["public"]
+        cs.send(send_encrypted(encrypt_aes(code.encode("utf-8")), user_pub)\
+                .encode("utf-8"))
 
 s = socket.socket()
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -75,11 +111,11 @@ print(f"[*] Listening as {SERVER_HOST}:{SERVER_PORT}")
 while True:
     cli_socket, cli_addr = s.accept()
     print(f"[+] {cli_addr} connected.")
-
+    cli_socket.send(SERVER_RSA.public_key().export_key())
     first_resp = cli_socket.recv(1024).decode("utf-8")
     username = ""
     if first_resp == "!signup":
-        handle_command("!signup".encode("utf-8"), cli_socket)
+        handle_command("!signup", cli_socket)
     else:
         username = first_resp
     username = cli_socket.recv(1024).decode("utf-8") if not username else username
@@ -87,14 +123,18 @@ while True:
     if username in users and username not in authenticated_users:
         password = users[username]["passw"]
         salt = users[username]["salt"]
-        challenge = encrypt("OK".encode("utf-8"), password)       
-        challenge_string = f"{b64encode(salt).decode("utf-8")}|{b64encode(challenge).decode("utf-8")}"
-        cli_socket.send(encrypt(challenge_string.encode("utf-8")))
+        user_pub = users[username]["public"]
+        challenge, _ = encrypt_aes("OK".encode("utf-8"), password)       
+        challenge_string =\
+        f"{b64encode(salt).decode("utf-8")}|{b64encode(challenge).decode("utf-8")}"
+        cli_socket.send(send_encrypted(
+            encrypt_aes(challenge_string.encode("utf-8")), user_pub
+            ).encode("utf-8"))
 
         response = cli_socket.recv(1024).decode("utf-8")
         if response == "OK":
             client_sockets.add(cli_socket)
-            authenticated_users.append(username)
+            authenticated_users[username] = cli_socket
             f_codes[username] = generate_sha256()
             print(f"[+] {username} authenticated successfully.")
             t = Thread(target=listen_for_client, args=(cli_socket, username), daemon=True)
