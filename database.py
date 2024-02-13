6490f133f924821df6c4010ce55e7daaf15185ea
cuewen <105962560+cuewen@users.noreply.github.com>
2024-02-13 19:23:27 +0700

    interaction with postgres db

---
+++ database.py
@@ -0,0 +1,40 @@
+import psycopg2
+import psycopg2.extras
+
+def connect(password):
+    conn = psycopg2.connect(
+        database="Chat-project", 
+        user="postgres", 
+        password=password, 
+        host="localhost", 
+        port="5432"
+        )
+    return conn
+
+def get_user(conn, name):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute(
+        "SELECT * FROM public.users WHERE name = %s", 
+        (name,)
+        )
+    user = cur.fetchone()
+    cur.close()
+    return user
+
+def get_all_users(conn):
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute(
+        "SELECT * FROM public.users"
+    )
+    users = cur.fetchall()
+    cur.close()
+    return users
+
+def add_user(conn, name, password, salt, public_key):
+    cur = conn.cursor()
+    cur.execute(
+    "INSERT INTO users (name, password, salt, public_key) VALUES (%s, %s, %s, %s)", 
+    (name, psycopg2.Binary(password), psycopg2.Binary(salt), public_key.decode('utf-8'))
+)
+    conn.commit()
+    cur.close()
