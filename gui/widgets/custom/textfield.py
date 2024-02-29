b9ec47309b5f612c31cc3be1e31bd503e3c3c574
cuewen <105962560+cuewen@users.noreply.github.com>
2024-02-29 11:18:40 +0700

    minor changes

--- gui/widgets/custom/textfield.py
+++ gui/widgets/custom/textfield.py
@@ -7,7 +7,7 @@ from PySide6.QtWidgets import (
     QPushButton, 
     QHBoxLayout,
     )
-from PySide6.QtGui import QPainter, QTextDocumentFragment, QIcon, QTextBlockFormat
+from PySide6.QtGui import QPainter, QTextDocumentFragment, QIcon
 from PySide6.QtCore import Qt, QSize
 
 class TextField(QLineEdit):
