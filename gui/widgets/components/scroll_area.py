c7e88d93243fa2c1e34702391317bef14cdc3936
cuewen <105962560+cuewen@users.noreply.github.com>
2024-03-10 20:50:42 +0700

    Scrollbar style

--- gui/widgets/components/scroll_area.py
+++ gui/widgets/components/scroll_area.py
@@ -6,6 +6,27 @@ class ScrollArea(QScrollArea):
         self.verticalScrollBar().rangeChanged.connect(self.handleRangeChanged)
         self.old_max = self.verticalScrollBar().maximum()
         self.relative_position = self.verticalScrollBar().value() / self.old_max if self.old_max > 0 else 0
+        self.setStyleSheet(
+            """
+            QScrollArea{
+                border: none;
+                border-bottom: 1px solid #2e2e2e;
+            }
+            QScrollBar:vertical {
+                width: 9px;
+                background-color: #1e1e1e;
+                margin-right: 5px;
+            }
+            QScrollBar::handle:vertical {
+                background: #5e5e5e;
+                min-height: 20px;
+                border-radius: 2px;
+            }
+            QScrollBar::add-page:vertical, QScrollBar::sub-page:vertical {
+                background-color: #1e1e1e;
+            }
+            """
+            )
 
     def wheelEvent(self, event):
         self.old_max = self.verticalScrollBar().maximum()
