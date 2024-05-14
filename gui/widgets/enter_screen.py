5abeb1f2a4f101f9668aa1eabe30630fde3f99aa
cuewen <105962560+cuewen@users.noreply.github.com>
2024-05-15 06:14:59 +0700

    Add change server functionality

--- gui/widgets/enter_screen.py
+++ gui/widgets/enter_screen.py
@@ -1,11 +1,19 @@
+from configparser import ConfigParser
+
 from PySide6.QtWidgets import (
     QWidget,
     QGridLayout,
     QVBoxLayout,
     QPushButton,
+    QToolButton,
     )
+from PySide6.QtGui import QIcon
+from PySide6.QtCore import Qt, Signal
+
+from .components import CustomMenu
 
 class EnterWidget(QWidget):
+    reinit = Signal()
     def __init__(self, stacked_layout, s):
         super().__init__()
         self.stacked_layout = stacked_layout
@@ -16,12 +24,42 @@ class EnterWidget(QWidget):
         self.sign_in_button = QPushButton("Sign In")
         self.sign_up_button = QPushButton("Sign Up")
         self.sign_up_button.setObjectName("reg")
+        self.options = QToolButton()
+        self.options.setFixedSize(30, 30)
+        self.options.setIcon(QIcon("./public/options.png"))
+
+        self.menu = CustomMenu(offset=True)
+
+        self.config = ConfigParser()
+        self.config.read("./gui/config.ini")
+
+        for server in self.config.sections():
+            host = self.config.get(server, "host")
+            port = self.config.getint(server, "port")
+            if server == "Current":
+                self.menu.add_action(f"Current server: {host}:{port}", 
+                                     status=False)
+                self.menu.add_separator()
+                continue
+            
+            addr = f"{host}:{port} ({server})"
+
+            self.menu.add_action(addr, lambda: self.change_server(host, port))
+
+        self.menu.add_action("Connect to...")
+        self.options.setMenu(self.menu)
+        self.options.setPopupMode(QToolButton.InstantPopup)
+
         buttons = QVBoxLayout()
         buttons.addWidget(self.sign_in_button)
         buttons.addSpacing(20)
         buttons.addWidget(self.sign_up_button)
+
+        option = QVBoxLayout()
+        option.addWidget(self.options, alignment=Qt.AlignTop | Qt.AlignRight)
         
         layout.addItem(buttons, 1, 1)
+        layout.addItem(option, 0, 2)
 
         layout.setColumnStretch(0, 1)
         layout.setColumnStretch(1, 1)
@@ -41,10 +79,20 @@ class EnterWidget(QWidget):
                 font-weight: 600;
                 outline: none;
             }
-            QPushButton:hover{
+            QToolButton{
+                background-color: #2e2e2e;
+                border-radius: 6px;
+                outline: none;
+            }
+            QPushButton:hover, QToolButton:hover{
                 background-color: #3e3e3e;
             }
-            QPushButton:pressed{background-color: #5e5e5e;}
+            QPushButton:pressed, QToolButton:pressed{
+                background-color: #5e5e5e;
+                }
+            QToolButton::menu-indicator{
+                image: none;
+            }
             #reg{
                 background-color: #1e1e1e;
                 border: 2px solid #2e2e2e;
@@ -67,3 +115,14 @@ class EnterWidget(QWidget):
     def on_sign_up_clicked(self):
         self.s.send("/signup".encode())
         self.stacked_layout.setCurrentIndex(2)
+
+    def change_server(self, host, port):
+        """Changes 'Current' section's options and restarts UI."""
+
+        self.config.set("Current", "host", host)
+        self.config.set("Current", "port", str(port))
+
+        with open("./gui/config.ini", "w") as cfg:
+            self.config.write(cfg)
+
+        self.reinit.emit()
