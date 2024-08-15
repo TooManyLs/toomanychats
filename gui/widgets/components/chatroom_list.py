456240889bbb4b3f5988583899b1839ad29d1634
cuewen <105962560+cuewen@users.noreply.github.com>
2024-08-15 12:12:07 +0700

    Visual fix for scrollability

--- gui/widgets/components/chatroom_list.py
+++ gui/widgets/components/chatroom_list.py
@@ -3,7 +3,7 @@ from PySide6.QtWidgets import QWidget, QVBoxLayout, QSizePolicy, QSpacerItem
 
 
 class ChatRoomList(QWidget):
-    collapsed_width = 65
+    collapsed_width = 70
     threshold_width = 240
 
     def __init__(self, window) -> None:
@@ -12,11 +12,14 @@ class ChatRoomList(QWidget):
         self.constrained_by_size = False
         self.main_window = window
         self.list = QVBoxLayout(self)
+        self.list.setDirection(QVBoxLayout.Direction.BottomToTop)
         self.list.setContentsMargins(0,0,0,0)
         self.list.setSpacing(0)
 
         self.list.addItem(QSpacerItem(0, 0, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding))
-    
+        
+        self.setStyleSheet("background-color: #161616;")
+
     def collapse_toggle(self) -> None:
         self.is_collapsed = not self.is_collapsed
         print("Collapsed" if self.is_collapsed else "Expanded")
