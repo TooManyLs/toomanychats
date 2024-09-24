7fd034c2162a42d1a5ba0c9e7341fe0375fa966b
TooManyLs <dimonzin@icloud.com>
2024-09-24 19:11:47 +0700

    Improved install script, works only on linux (win script broken)

--- setup.py
+++ setup.py
@@ -2,10 +2,24 @@ import os
 import subprocess
 import sys
 from configparser import ConfigParser
+from io import StringIO
+
+from encryption import encrypt_aes
 
 
 db_password = input('Enter the password for database: ')
-db_name = input('Enter the desired name for your Chat database: ')
+while True:
+    db_name = input('Enter the desired name for your Chat database: ').lower()
+    if not db_name.isalpha():
+        print("Only lowercase latin chars accepted.")
+        continue
+    break
+
+encryption_key = input("""
+Enter encryption key for your config.
+YOU'LL NEED TO ENTER IT EACH TIME YOU RUN YOUR SERVER!!!
+(16-characters max, overflow will be cut): 
+""").rjust(16, "0")[:16]
 
 if os.name == 'posix':
 
@@ -36,14 +50,26 @@ if os.name == 'posix':
 
         os.environ['PM'] = pms[pm_choice]
 
+        if hasattr(sys, 'real_prefix') or (hasattr(sys, 'base_prefix') and sys.base_prefix != sys.prefix):
+            print("Virtual environment:", sys.prefix)
+        else:
+            print("No virtual environment detected.")
         subprocess.run(['bash', 'install_linux.sh'], cwd='scripts')
 else:
     subprocess.run(['install_win.bat', db_password, db_name], cwd='scripts')
+
 
 config = ConfigParser()
 config.add_section('Database')
 config.set('Database', 'DB_PASSWORD', db_password)
 config.set('Database', 'DB_NAME', db_name)    
 
-with open('server.ini', 'w') as cfg:
-    config.write(cfg)
+output = StringIO()
+config.write(output)
+conf = output.getvalue()
+
+encrypted_conf, _ = encrypt_aes(conf.encode(), key=encryption_key.encode())
+
+with open('server.conf.enc', 'wb') as cfg:
+    cfg.write(encrypted_conf)
+
