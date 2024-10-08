4250749855df722557f4c70a406a2e09efad9e19
TooManyLs <dimonzin@icloud.com>
2024-10-08 14:56:52 +0700

    Display remaining time and sender's name; Tidy up

--- gui/widgets/components/video_widget.py
+++ gui/widgets/components/video_widget.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from typing import Optional
 
-from PySide6.QtGui import QIcon, QPixmap, QPainter
-from PySide6.QtWidgets import QLabel, QWidget, QGridLayout, QPushButton
+from PySide6.QtGui import QFontMetrics, QIcon, QPixmap, QPainter
+from PySide6.QtWidgets import (
+        QLabel, QWidget, QGridLayout,
+        QPushButton, QSizePolicy,
+)
 from PySide6.QtMultimediaWidgets import QVideoWidget
 from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
 from PySide6.QtCore import Qt, QSize, QTimer, QObject
@@ -24,14 +27,16 @@ class MediaPlayer(QMediaPlayer):
 
 class VideoWidget(QVideoWidget):
     def __init__(
-            self, file: str, parent: QWidget,
+        self, file: str, parent: QWidget, name: str = "",
             timestamp: datetime = datetime.now()
     ) -> None:
         super().__init__(parent)
         
         self.file = file
         self.par = parent
+        self.name_text = name
         self.player = MediaPlayer(file, self)
+        self.setCursor(Qt.CursorShape.PointingHandCursor)
 
         self.player.mediaStatusChanged.connect(self.handleMediaStatus)
 
@@ -45,31 +50,57 @@ class VideoWidget(QVideoWidget):
         self.playpause.setFixedSize(40, 40)
         self.playpause.clicked.connect(self.play_pause_toggle)
         self.__round_corners(self.playpause, 20)
+        self.playpause.setStyleSheet("background-color: #2e2e2e;")
         self.playpause.hide()
 
         self.time_text = timestamp.strftime("%I:%M %p")
         self.time = QLabel(self.time_text)
         self.time.setFixedSize(63, 23)
-        self.__round_corners(self.time, 12)
-
-        self.playpause.setStyleSheet("background-color: #2e2e2e;")
-        self.time.setStyleSheet(
+        self.__round_corners(self.time, 10)
+        self.time.setObjectName("vid_overlay")
+        self.setStyleSheet(
             """
+            #vid_overlay{
                 background-color: #2e2e2e;
                 padding: 3px;
                 color: white;
+            }
+            QPushButton {
+                font-weight: 700;
+            }
             """
         )
 
-        self.controls.addWidget(self.playpause, 1, 1, 2, 3,
+        self.timer_label = QLabel("")
+        self.timer_label.setFixedHeight(23)
+        self.timer_label.setObjectName("vid_overlay")
+
+        if name:
+            self.name = QPushButton(self.name_text)
+            self.name.clicked.connect(lambda: print(f"pushed {self.name_text}"))
+            nm_metric = QFontMetrics(self.name.font())
+            width = nm_metric.horizontalAdvance(self.name_text) * 1.3
+            self.name.setFixedSize(int(width), 23)
+            self.__round_corners(self.name, 10)
+            self.name.setObjectName("vid_overlay")
+
+            self.controls.addWidget(self.name, 0, 0, 1, 1,
+                                    alignment=Qt.AlignmentFlag.AlignLeft 
+                                    | Qt.AlignmentFlag.AlignTop)
+
+        self.controls.addWidget(self.timer_label, 0, 3, 1, 1,
+                                alignment=Qt.AlignmentFlag.AlignTop
+                                | Qt.AlignmentFlag.AlignRight)
+        self.controls.addWidget(self.playpause, 1, 0, 1, -1,
                                 alignment=Qt.AlignmentFlag.AlignCenter)
-        self.controls.addWidget(self.time, 2, 3, 1, 1,
+        self.controls.addWidget(self.time, 2, 3, 1, -1,
                                 alignment=Qt.AlignmentFlag.AlignBottom
                                 | Qt.AlignmentFlag.AlignRight)
 
         self.timer = QTimer()
         self.timer.setSingleShot(True)
         self.timer.timeout.connect(self.playpause.hide)
+        self.player.positionChanged.connect(self.update_timer)
 
     def __round_corners(self, widget: QWidget, radius: float):
         mask = QPixmap(widget.size())
@@ -111,6 +142,17 @@ class VideoWidget(QVideoWidget):
         if self.size() != old_size:
             self.__round_corners(self, 12)
 
+    def update_timer(self):
+        remaining_time = self.duration - self.player.position()
+        minutes, seconds = divmod(remaining_time // 1000, 60)
+        time_string = f"{minutes:02}:{seconds:02}"
+        self.timer_label.setText(time_string)
+
+        t_metric = QFontMetrics(self.timer_label.font())
+        width = t_metric.horizontalAdvance(time_string) * 1.4
+        self.timer_label.setFixedWidth(int(width))
+        self.__round_corners(self.timer_label, 10)
+
     def handleMediaStatus(self, status):
         if status == QMediaPlayer.MediaStatus.LoadedMedia: 
             resolution: QSize = self.videoSink().videoSize()
@@ -118,10 +160,8 @@ class VideoWidget(QVideoWidget):
             self.source_height = resolution.height()
             self.compute_size()
             
-            seconds = round(self.player.duration() / 1000)
-            self.short = seconds <= 59
-            minutes, seconds = divmod(seconds, 60)
-            duration = f"{minutes}:{seconds:02}"
+            self.duration = self.player.duration()
+            self.short = self.duration // 1000 <= 59
 
             if self.short:
                 self.player.play()
